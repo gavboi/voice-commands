caa7b6a59f20130ddbf58e7528d67018cf8862bb
gavboi <gj1087@hotmail.com>
2024-05-24 17:34:08 -0400

    commands to take notes

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # voice-commands
 Random commands controlled by voice
 
+## Usage
 
-### Ramblings
+Run `voice_recognition.py [name]`. Provide `name` if you want to use another keyword other than the default: "computer". Starting a phrase with `name` will tell the script to attempt to process what you say as a command.
 
-#### Ideas
-[Background recongniton](https://github.com/Uberi/speech_recognition/blob/master/examples/background_listening.py)
+### Commands
+
+#### Write a Note
+Tell `name` to "write" something, and it will create a text file with that in it.
+
+## Notes
+
+You seem to need a not terrible microphone for it to work with any consistency.
--- voice_recognition.py
+++ voice_recognition.py
@@ -1,15 +1,63 @@
+import time
+import sys
+
 import speech_recognition as sr
 
+
+# process arg for name
+name = 'computer'
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+
+# processing options
+commands = []
+def note(content):
+    with open(f'note-{str(time.time()).split(".")[0]}.txt', 'w') as file:
+        file.write(content)
+commands.append({
+    'name': 'Note',
+    'phrases': ['write', 'write down', 'write down that', 'take a note to'],
+    'action': note
+})
+
+# initialize
 r = sr.Recognizer()
+m = sr.Microphone()
+running = True
+processed = False
 
-with sr.Microphone() as source:
-    print('Adjusting for ambient noise...')
+# prepare recognizer
+with m as source:
+    print('(Adjusting for ambient noise)')
     r.adjust_for_ambient_noise(source, duration=3)
-    print('Say something!')
-    audio = r.listen(source)
+print('(Ready)')
 
-try:
-    output = r.recognize_google(audio)
-    print(f'You said: {output}')
-except sr.UnknownValueError:
-    print('Could not understand')
+# loop
+while running:
+    with m as source:
+        audio = r.listen(source)
+    processed = False
+    print('(Processing)')
+    try:
+        output = r.recognize_google(audio)
+        if output.lower().startswith(name) or output.lower().startswith(f'hey {name}'):
+            print(f'** {output}')
+            # remove name
+            output = output[output.lower().index(name)+len(name):].strip()
+            for command in commands:
+                for phrase in sorted(command['phrases'], key=len, reverse=True):
+                    if output.lower().startswith(phrase):
+                        content = output[output.lower().index(phrase)+len(phrase):].strip()
+                        print(f'{command["name"]}: {content}')
+                        command['action'](content)
+                        processed = True
+                        break
+                if processed:
+                    break
+        else:
+            print(f'{output}')
+            
+    except sr.UnknownValueError:
+        print('(Could not understand!)')
+    finally:
+        print('(Listening)')
